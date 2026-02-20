60085b6bfcb884a022d7476063ccebe7cb85e485
Aadesh Bhattarai <aadeshwins55@gmail.com>
2026-02-20 13:24:54 -0700

    fixed placement on screen of lattice

--- app/src/main/java/com/audioreactive/ui/screens/LatticeScreen.kt
+++ app/src/main/java/com/audioreactive/ui/screens/LatticeScreen.kt
@@ -1,9 +1,11 @@
 package com.audioreactive.ui.screens
 
+import android.util.DisplayMetrics
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import com.csci448.abhattarai.reactortest.points.AnimatedLatticeDisplay
 import com.csci448.abhattarai.reactortest.points.lattice
 
@@ -12,12 +14,17 @@ fun VisualizerLattice(modifier: Modifier) {
 
     modifier
         .fillMaxSize()
+    val context = LocalContext.current
+    val displayMetrics: DisplayMetrics = context.resources.displayMetrics
+    val screenWidthPx = displayMetrics.widthPixels
+    val screenHeightPx = displayMetrics.heightPixels
 
 
     val l = remember {
+
         lattice(
-            x = 540, // idk why I have to offset it so wierd
-            y = 1080,
+            x = screenWidthPx / 2 ,
+            y = screenHeightPx / 2,
             width = 0,
             height = 1080,
             frequencyBand = 0
