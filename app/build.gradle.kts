0eda8f2680f2f62bac0cfd6d0fd4747327f4da23
Samvat Dangol <samvat04@hotmail.com>
2026-04-13 19:55:01 -0600

    Forgot to add this file to git for the color picker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,15 +34,18 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlin {
         compilerOptions {
             jvmTarget = JvmTarget.JVM_11
         }
     }
+
     buildFeatures {
         compose = true
     }
@@ -67,6 +70,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.compose.color.picker)
     implementation(libs.firebase.crashlytics)
     implementation(libs.firebase.analytics)
 
